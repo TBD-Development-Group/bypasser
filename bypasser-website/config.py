cafc739a23c5e455bff36c3b10c9a72adfdc02cb
TBD-Development-Group <boumaazahamza30@gmail.com>
2025-05-16 18:46:19 +0200

    Update config.py

--- bypasser-website/config.py
+++ bypasser-website/config.py
@@ -1,1 +1,16 @@
+APIs = [
+    "https://us-central1-my-app-12345.cloudfunctions.net/app",  # Shareus API
+    "https://api.bypass.vip",  # Universal Bypass API
+    "https://bypass.bot.nu/bypass2",  # Other Bypass API
+    "https://api.gofile.io"  # Gofile API
+]
 
+Creds = [
+    "youremail@gmail.com",  # For unified login
+    "yourpassword"          # For unified login
+]
+
+sys = {
+    "MAX_FILE_SIZE": 500,  # In MBs
+    "MIN_FILE_SIZE": 0     # In MBs
+}
